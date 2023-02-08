9e4304176b5e65e9d997fa05987d8df23fa8efbb
mmednik <mmednik@gmail.com>
2023-02-08 11:18:30 -0300

    add Maven Pro google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;700&display=swap" rel="stylesheet" />
       <body>
         <Main />
         <NextScript />
